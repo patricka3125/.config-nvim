1be3e53b88e421ba29a7b5873276f8263fe645e9
Patrick L <liao.patrick@gmail.com>
2026-03-02 01:35:52 -0800

    Make git diffview look better

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,5 +11,16 @@ return {
 		keys = {
 			{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
 		},
+		config = function()
+			local hl = vim.api.nvim_set_hl
+
+			-- Added lines (Green)
+			hl(0, "NeogitDiffAdd", { fg = "#a7c080", bg = "#333c30" })
+			hl(0, "NeogitDiffAddHighlight", { fg = "#d7ffaf", bg = "#3d4839", bold = true })
+
+			-- Removed lines (Red) - for a consistent look
+			hl(0, "NeogitDiffDelete", { fg = "#e67e80", bg = "#3c3431" })
+			hl(0, "NeogitDiffDeleteHighlight", { fg = "#e67e80", bg = "#4c3733", bold = true })
+		end,
 	},
 }
